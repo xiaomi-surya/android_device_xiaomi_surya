f42c34525bfbca2a49e5b7ab2c4499a2713bda75
kamikaonashi <thorschi93@gmx.at>
2024-01-07 08:27:25 +0100

    surya: change xiaomi parts
    change xiaomi parts to https://github.com/AlphaDroid-devices/device_xiaomi_sm8350-common/tree/alpha-13/parts
    
    and adapt it to work on surya and drop unneccesary stuff

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,18 +20,25 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
+
+import androidx.preference.PreferenceManager;
+import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.touchsampling.TouchSamplingUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        ThermalUtils.startService(context);
+        TouchSamplingUtils.restoreSamplingValue(context);
+        RefreshUtils.startService(context);        
     }
 }
