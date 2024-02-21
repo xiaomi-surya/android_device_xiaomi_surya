62e93c2c1e6dadb0bcdabb1b1cb7d669784527a5
Aminz <3411211078.aminuddin.ihsan@gmail.com>
2024-02-21 13:01:46 +0700

    Surya : Revert Enable project quotas and casefolding

--- device.mk
+++ device.mk
@@ -16,8 +16,7 @@ $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
